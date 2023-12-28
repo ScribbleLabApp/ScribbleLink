c06805a39825cfbbc8528c955f64859bc3cfdbff
N3v1 <n3v1010@gmail.com>
2023-12-28 12:49:50 +0100

    Initial Commit

---
+++ ScribbleLink/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  ScribbleLink
+//
+//  Created by Nevio Hirani on 28.12.23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ ScribbleLink/ScribbleLinkApp.swift
@@ -0,0 +1,17 @@
+//
+//  ScribbleLinkApp.swift
+//  ScribbleLink
+//
+//  Created by Nevio Hirani on 28.12.23.
+//
+
+import SwiftUI
+
+@main
+struct ScribbleLinkApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
